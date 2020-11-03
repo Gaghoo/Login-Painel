98525af2f144c6cda6720490264b37adb59ab30c
Helom Zive <helomzive@gmail.com>
2020-11-03 14:23:06 -0300

    concluido por hora, vou depois para atualização

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,66 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-app-bar app dark color="indigo darken-3">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Login Page</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text rounded class="mx-1">
+                <v-icon left>mdi-home-outline</v-icon>
+                <span>Home</span>
+            </v-btn>
+            <v-btn text rounded class="mx-1">
+                <v-icon left>mdi-card-account-details-outline</v-icon>
+                <span>About Us</span>
+            </v-btn>
+            <v-btn text rounded class="mx-1">
+                <v-icon left>mdi-account-plus-outline</v-icon>
+                <span>Register</span>
+            </v-btn>
+        </v-app-bar>
+        <v-main class="indigo lighten-4 d-flex justify-center align-center">
+            <div class="d-flex justify-center">
+                <v-card width="500">
+                    <v-card-title>Login</v-card-title>
+                    <v-card-text>
+                        <v-text-field
+                            label="Username"
+                            outlined
+                            prepend-inner-icon="mdi-account-circle-outline"
+                            shaped
+                        ></v-text-field>
+                        <v-text-field
+                            label="Password"
+                            outlined
+                            prepend-inner-icon="mdi-lock-outline"
+                            shaped
+                            type="password"
+                            append-icon="mdi-eye-off-outline"
+                        ></v-text-field>
+                    </v-card-text>
+                    <v-divider/>
+                    <v-card-actions>
+                        <v-btn color="indigo" elevation="6" rounded class="mx-auto my-6">
+                            <v-icon left>mdi-login-variant</v-icon>
+                            <span>Login</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </div>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
 
-  data: () => ({
-    //
-  }),
+    },
+
+    data: () => ({
+        
+    }),
 };
 </script>
